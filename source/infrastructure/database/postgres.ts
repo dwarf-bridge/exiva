89e4cc84189b0f287f9cb7b0fc15f8fed7e66149
Nícolas 'Eowfenth' Deçordi <eowfenth.services@gmail.com>
2021-11-10 19:21:19 -0300

    chore: setup Postgres client to have a health-check before trying to query it;

--- source/infrastructure/database/postgres.ts
+++ source/infrastructure/database/postgres.ts
@@ -1,18 +1,39 @@
 import { Database } from '../../config'
 import knex, { Knex } from 'knex'
 import { injectable } from 'tsyringe'
+import { wait } from '../../main/utils/wait'
 
 @injectable()
 export class Postgres {
     public instance: Knex
 
     constructor() {
-        const config = process.env.NODE_ENV === 'production' ? Database.config.production : Database.config.development
+        const config =
+            process.env.NODE_ENV === 'production'
+                ? Database.config.production
+                : Database.config.development
         this.instance = knex({
             client: 'postgresql',
             connection: config.connection,
             pool: config.pool,
             acquireConnectionTimeout: 2000,
+            ...config,
         })
+    }
+
+    public async isAvailable() {
+        const MAX_RETRIES = 5
+        for (let trial = 1; trial <= MAX_RETRIES; trial++) {
+            try {
+                console.log(`infra:database | connection trial no. ${trial}`)
+                await this.instance.raw('SELECT NOW();')
+                console.info('infra:database | connected')
+                return true
+            } catch (err) {
+                await wait(2000)
+            }
+        }
+
+        return false
     }
 }
