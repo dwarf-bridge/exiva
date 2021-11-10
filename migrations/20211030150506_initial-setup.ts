516df954371cbef42eba6619f064cdcf7beba487
Nícolas 'Eowfenth' Deçordi <eowfenth.services@gmail.com>
2021-11-09 21:54:52 -0300

    chore: adjust migration to have proper fields for highscore and game_worlds

--- migrations/20211030150506_initial-setup.ts
+++ migrations/20211030150506_initial-setup.ts
@@ -70,7 +70,11 @@ export async function up(knex: Knex): Promise<void> {
 
             table.timestamps(true, true)
 
-            table.string('name').notNullable().comment('Game World name')
+            table.string('name')
+                .notNullable()
+                .unique()
+                .notNullable()
+                .comment('Game World name')
             table
                 .enum('battle_eye', [
                     'fully-protected',
@@ -97,11 +101,68 @@ export async function up(knex: Knex): Promise<void> {
                 .specificType('server_conditions', 'text ARRAY')
                 .notNullable()
                 .comment('Conditions applied to this game server')
+            table
+                .uuid('merged_into')
+                .comment("New World's Id")
+            table
+                .date('merged_at')
+                .comment('Date of Merge')
+            table
+                .specificType('server_titles', 'text ARRAY')
+                .notNullable()
+                .comment('Server titles earned by World Quests')
+            table
+                .boolean('is_special_world')
+                .comment('Mark for special worlds like Tournament Worlds')
         })
 
     /**
      * Highscores
      */
+     await knex.schema
+     .withSchema(Database.schema)
+     .createTable('highscore_categories', (table) => {
+        const columns = schema(table)
+        columns.primaryUuid()
+
+        table.timestamps(true, true)
+
+        table
+        .enum('category', [ 
+            'achievements',
+            'axe-fighting',
+            'charm-points',
+            'club-fighting',
+            'distance-fighting',
+            'drome-score',
+            'experience',
+            'fishing',
+            'fist-fighting',
+            'goshnar-taints',
+            'loyalty-points',
+            'magic-level',
+            'shielding',
+            'sword-fighting',
+        ]).notNullable()
+        table.integer('reference').notNullable()
+    })
+
+    await knex('highscore_categories').withSchema(Database.schema).insert([
+        { category: 'achievements', reference: 1 },
+        { category: 'axe-fighting', reference: 2 },
+        { category: 'charm-points', reference: 3 },
+        { category: 'club-fighting', reference: 4 },
+        { category: 'distance-fighting', reference: 5 },
+        { category: 'experience', reference: 6 },
+        { category: 'drome-score', reference: 14 },
+        { category: 'fishing', reference: 7 },
+        { category: 'fist-fighting', reference: 8 },
+        { category: 'goshnar-taints', reference: 9 },
+        { category: 'loyalty-points', reference: 10 },
+        { category: 'magic-level', reference: 11 },
+        { category: 'shielding', reference: 12 },
+        { category: 'sword-fighting', reference: 13 },
+    ])
     await knex.schema
         .withSchema(Database.schema)
         .createTable('highscores', (table) => {
@@ -114,7 +175,7 @@ export async function up(knex: Knex): Promise<void> {
                 .integer('rank')
                 .notNullable()
                 .comment('Player rank at the time')
-            table.string('name').notNullable().comment("Player's name")
+            table.string('character_name').notNullable().comment("Player's name")
             table.integer('level').notNullable().comment("Player's level")
             table.bigInteger('points').notNullable().comment("Player's point")
             table.date('date').notNullable().comment('Date of current rank')
@@ -125,6 +186,10 @@ export async function up(knex: Knex): Promise<void> {
                 .references('id')
                 .inTable(`${Database.schema}.game_worlds`)
             table.integer('vocation').notNullable().comment("Player's vocation")
+            table
+            .integer('category')
+            .notNullable()
+            .comment("This rank category")
         })
 
     // Online Characters Entries
@@ -161,4 +226,7 @@ export async function down(knex: Knex): Promise<void> {
     await knex.schema.withSchema(Database.schema).dropTable('highscores')
     await knex.schema.withSchema(Database.schema).dropTable('game_worlds')
     await knex.schema.withSchema(Database.schema).dropTable('vocations')
+    await knex.schema.withSchema(Database.schema).dropTable('online_checks')
+    await knex.schema.withSchema(Database.schema).dropTable('actions')
+    await knex.schema.withSchema(Database.schema).dropTable('highscore_categories')
 }
