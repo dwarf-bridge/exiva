81d34a414cecc1b5508c3a32a5cdcc5aae6ad11e
Nícolas 'Eowfenth' Deçordi <eowfenth.services@gmail.com>
2021-11-09 21:59:31 -0300

    chore: add DI for Highscore and World collectors

--- source/di/container.ts
+++ source/di/container.ts
@@ -3,10 +3,14 @@ import { tokens } from './tokens'
 import { Exiva } from '../application/services/exiva'
 import { Fetch } from '../application/services/fetch';
 import { Postgres } from '../infrastructure/database/postgres';
+import { HighscoreCollector } from '../application/services/highscore';
+import { WorldCollector } from '../application/services/worlds';
 
 const Container = container.createChildContainer()
 
 Container.registerSingleton(tokens.Exiva, Exiva)
+Container.register(tokens.HighscoreCollector, HighscoreCollector)
+Container.register(tokens.WorldCollector, WorldCollector)
 Container.registerSingleton(tokens.Fetch, Fetch)
 Container.registerSingleton(tokens.Postgres, Postgres)
 
--- source/di/tokens.ts
+++ source/di/tokens.ts
@@ -1,5 +1,7 @@
 const tokens = {
     Exiva: Symbol.for('Exiva'),
+    HighscoreCollector: Symbol.for('HighscoreCollector'),
+    WorldCollector: Symbol.for('WorldCollector'),
     Fetch: Symbol.for('Fetch'),
     Postgres: Symbol.for('Postgres'),
 }
