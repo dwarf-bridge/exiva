6e6329d11aac2e52ba1b0f22eb8b2cea75876192
Nícolas 'Eowfenth' Deçordi <eowfenth.services@gmail.com>
2021-11-10 19:25:26 -0300

    fix: exiva where it could break on empty worlds and added game_world field to the check

--- source/application/services/exiva/index.ts
+++ source/application/services/exiva/index.ts
@@ -7,12 +7,12 @@ import { Postgres } from '../../../infrastructure/database/postgres'
 
 /**
  * Observations
- * 
+ *
  * It does cost like 50kb each run to save these information;
  * 7200 kb in a day;
  * 216000 kb in a month (216 mb of data storage each month for each server);
- * 
- * 
+ *
+ *
  * dwarfbridge.com/exiva?world=Nossobra
  * - Show a graph pointing to players by hour;
  * - Unique players at the World;
@@ -39,22 +39,19 @@ export type CharacterOnlineEntry = {
 @injectable()
 export class Exiva {
     private page: Page | null = null
-    private world: { name: string, id: string } | null = null;
+    private world: { name: string; id: string } | null = null
     private content: CharacterOnlineRow[] = []
+    private worlds: { name: string; id: string }[] = []
 
     constructor(@inject(tokens.Postgres) private db?: Postgres) {}
 
     public async collect(world: string): Promise<string | undefined> {
-        const worlds: {id: string, name: string}[] = await this.db?.instance
-        .select('id', 'name')
-        .from(`${Database.schema}.game_worlds`)!
-        this.world = worlds.filter(x => x.name === world).shift()!;
         try {
+            console.info(`service:exiva:collect | world: ${world}`)
             const result = await Fetch.get(
                 Config.ONLINE_CHECKING_URL + `${world}`
             )
-            const page = await result.text()
-            return page
+            return result.data
         } catch (err) {
             console.log(err)
         }
@@ -73,39 +70,50 @@ export class Exiva {
             const characters = set.map((character) => {
                 const char_row_node = this.page!(character).contents()
 
+                const character_name = char_row_node.eq(0).text()
+                const level = char_row_node.eq(1).text()
+
                 return {
-                    character_name: char_row_node.eq(0).text(),
-                    level: char_row_node.eq(1).text(),
+                    character_name,
+                    level,
                 }
             })
 
-            return characters;
+            return characters.filter(
+                (char) =>
+                    char.character_name.length <= 255 ||
+                    isNaN(Number(char.level))
+            )
         }
-    
+
         return []
     }
 
     public parse(raw: CharacterOnlineRowRaw[]): CharacterOnlineRow[] {
-        const content = raw.map(row => ({
+        const content = raw.map((row) => ({
             character_name: row.character_name.trim(),
-            level: Number(row.level.trim())
+            level: Number(row.level.trim()),
         }))
         this.content = content
         return content
     }
 
-    public async store() {
-        const execution_time = new Date();
+    public async store(world_id: string) {
+        const execution_time = new Date()
         if (this.content.length === 0) {
-            console.info('There is no player online');
-            return;
+            console.info('service:online_check | no online players')
+            return
         }
-        
-        await this.db?.instance<CharacterOnlineEntry>('collection.online_checks').insert(this.content.map(entry => ({
+
+        const characterList = this.content.map((entry) => ({
             character_name: entry.character_name,
             character_level: entry.level,
             online_at: execution_time,
-        })))
+            game_world: world_id,
+        }))
+        await this.db
+            ?.instance<CharacterOnlineEntry>('collection.online_checks')
+            .insert(characterList)
     }
 
     public emit() {
@@ -114,24 +122,21 @@ export class Exiva {
          * - Emit Action Event;
          * - Emit Data;
          * - Store Data (for now);
-         */    
+         */
     }
 
     public async run() {
-        const world = 'Nossobra'
-        console.log(`Gathering online checks for ${world} at ${new Date()}`)
-        const page = await this.collect(world)
-
-        if (page) {
-            console.time('online-checking')
-            const processed = this.process(page)
-            console.timeEnd('online-checking')
-            console.log(processed?.length)
-            console.log(processed)
-            this.parse(processed)
-            this.store();
+        const worlds: { id: string; name: string }[] = await this.db?.instance
+            .select('id', 'name')
+            .from(`${Database.schema}.game_worlds`)!
+        for (let world of worlds) {
+            const page = await this.collect(world.name)
+            if (page) {
+                const processed = this.process(page)
+                this.parse(processed)
+                await this.store(world.id)
+            }
         }
         this.emit()
-        console.log(`Online check for ${world} just completed at ${new Date()}`!)
     }
 }
