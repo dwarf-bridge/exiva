35d790de2ba10619866ae1082cf48b539085e574
Nícolas 'Eowfenth' Deçordi <eowfenth.services@gmail.com>
2021-11-10 19:26:35 -0300

    feat: setup start-up of app

--- source/main/index.ts
+++ source/main/index.ts
@@ -4,52 +4,81 @@ import { tokens } from '../di/tokens'
 import { Exiva } from '../application/services/exiva'
 import { HighscoreCollector } from '../application/services/highscore'
 import { WorldCollector } from '../application/services/worlds'
+import { Postgres } from '../infrastructure/database/postgres'
+import { Database } from '../config'
 import cron from 'node-cron'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const exiva = Container.resolve(tokens.Exiva) as Exiva
-const highscore = Container.resolve(tokens.HighscoreCollector) as HighscoreCollector
+const highscore = Container.resolve(
+    tokens.HighscoreCollector
+) as HighscoreCollector
 const worlds = Container.resolve(tokens.WorldCollector) as WorldCollector
+const database = Container.resolve(tokens.Postgres) as Postgres
 
 /**
  * É possível obter quem está caçando (top 1000)
  * porque os dados da tabela de highscore é atualizado;
  */
-console.info('Collector is now running...');
-// worlds.run()
-cron.schedule(
-    '0 0 9 * * *',
-    () => {
-        console.info('World Summaries is now running');
-        worlds.run()
-        console.info('World Summaries just completed');
-    },
-    {
-        scheduled: true,
-        timezone: 'America/Sao_Paulo',
-    }
-)
+console.info('service:collector')
+;(async () => {
+    const isAvailable = await database.isAvailable()
+    if (isAvailable) {
+        console.time('database:migrations')
+        await database.instance.migrate.latest()
+        console.timeEnd('database:migrations')
+        console.time('service:world')
+        const worldsList = await database.instance
+            .select('id', 'name')
+            .from(`${Database.schema}.game_worlds`)!
+        console.timeEnd('service:world')
+        if (!worldsList.length) {
+            const worlds = Container.resolve(
+                tokens.WorldCollector
+            ) as WorldCollector
+            await worlds.run()
+            console.info('service:collector:worlds:done')
+        }
 
-console.info("Highscore is running")
-highscore.setWorld('Nossobra').run()
-cron.schedule(
-    '*/20 * * * *',
-    () => {
-        highscore.setWorld('Nossobra').run()
-    },
-    {
-        scheduled: true,
-        timezone: 'America/Sao_Paulo',
+        // cron.schedule(
+        //     '0 0 9 * * *',
+        //     async () => {
+        //         console.time('services:worlds-collector')
+        //         await worlds.run()
+        //         console.timeEnd('services:worlds-collector')
+        //     },
+        //     {
+        //         scheduled: true,
+        //         timezone: 'America/Sao_Paulo',
+        //     }
+        // )
+        // cron.schedule(
+        //     '*/5 * * * *',
+        //     async () => {
+        //         console.time('services:highscore-collector')
+        //         await highscore.setWorld('Nossobra').run()
+        //         console.timeEnd('services:highscore-collector')
+        //     },
+        //     {
+        //         scheduled: true,
+        //         timezone: 'America/Sao_Paulo',
+        //     }
+        // )
+        cron.schedule(
+            '*/15 * * * *',
+            async () => {
+                console.time('services:exiva')
+                await exiva.run()
+                console.timeEnd('services:exiva')
+            },
+            {
+                scheduled: true,
+                timezone: 'America/Sao_Paulo',
+            }
+        )
+    } else {
+        process.exit(1)
     }
-)
-
-console.info("Exiva is running")
-cron.schedule(
-    '*/15 * * * *',
-    () => {
-        exiva.run()
-    },
-    {
-        scheduled: true,
-        timezone: 'America/Sao_Paulo',
-    }
-)
+})()
