bad9342dd04b7039fbd4ce59cb61341700849939
Nícolas 'Eowfenth' Deçordi <eowfenth.services@gmail.com>
2021-11-11 10:00:42 -0300

    chore: add cookie support for requests

--- source/application/services/fetch/index.ts
+++ source/application/services/fetch/index.ts
@@ -1,10 +1,14 @@
 import axios from 'axios'
 import config from '../../../config'
+import { wrapper } from 'axios-cookiejar-support';
+import { CookieJar } from 'tough-cookie';
 
 export class Fetch {
     static async get(url: string) {
+        const jar = new CookieJar();
+        const client = wrapper(axios.create({ jar }));  
         console.info(`service:fetch | req: ${url}`)
-        return axios(`${config.BASE_URL}${url}`, {
+        return client(`${config.BASE_URL}${url}`, {
             method: 'GET',
             headers: {
                 "Accept-Encoding": "gzip, deflate",
