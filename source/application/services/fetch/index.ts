f5a4f672e1519cbd5ea8ab2ab33d9ef9bffc8c46
Nícolas 'Eowfenth' Deçordi <eowfenth.services@gmail.com>
2021-11-11 10:50:39 -0300

    feat: add user-agents handler for requests

--- source/application/services/fetch/index.ts
+++ source/application/services/fetch/index.ts
@@ -2,15 +2,18 @@ import axios from 'axios'
 import config from '../../../config'
 import { wrapper } from 'axios-cookiejar-support';
 import { CookieJar } from 'tough-cookie';
+import UserAgent from 'user-agents';
 
 export class Fetch {
     static async get(url: string) {
+        const userAgent = new UserAgent();
         const jar = new CookieJar();
         const client = wrapper(axios.create({ jar }));  
         console.info(`service:fetch | req: ${url}`)
         return client(`${config.BASE_URL}${url}`, {
             method: 'GET',
             headers: {
+                "User-Agent": userAgent.toString(),
                 "Accept-Encoding": "gzip, deflate",
             },
         })
